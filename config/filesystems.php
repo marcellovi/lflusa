c6c8a777b048d5dbc7a236cd5500a9c5d9cd8b51
Marcello F. Vieira <marcello.strategy@gmail.com>
2025-11-16 09:33:25 -0600

    change filesystem for Hostinger env

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,10 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            //'root' => storage_path('app'),
+            'root' => public_path('storage'), // Files will be stored in public/storage
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
             'throw' => false,
         ],
 
